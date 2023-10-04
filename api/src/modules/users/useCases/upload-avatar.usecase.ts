9adc10234d8ca1dcbcaeadafcef4826150e97e76
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-10-04 16:46:00 -0300

    refactor: creating the update role usecase and separating responsibility

--- api/src/modules/users/useCases/upload-avatar.usecase.ts
+++ api/src/modules/users/useCases/upload-avatar.usecase.ts
@@ -1,29 +1,29 @@
-// import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
-// import { IUserRepository } from '../repositories/user.repository';
-// import { extname } from 'path';
+import { IUserRepository } from '../repositories/user.repository';
+import { extname } from 'path';
 
-// @Injectable()
-// export class UploadAvatarUserUseCase {
-//   constructor(
-//     private storage: IStorage,
-//     private userRepository: IUserRepository,
-//   ) {}
+@Injectable()
+export class UploadAvatarUserUseCase {
+  constructor(
+    private storage: IStorage,
+    private userRepository: IUserRepository,
+  ) {}
 
-//   async execute(data: AvatarDTO) {
-//     const extFile = extname(data.file.originalname);
-//     const transformName = `${data.idUser}${extFile}`;
-//     /*
-//       originalname = avatar.png
-//       originalname = transformName (892349234789234923489234.png)
-//      */
-//     data.file.originalname = transformName;
-//     const file = await this.storage.upload(data.file, 'avatar');
+  async execute(data: AvatarDTO) {
+    const extFile = extname(data.file.originalname);
+    const transformName = `${data.idUser}${extFile}`;
+    /*
+      originalname = avatar.png
+      originalname = transformName (892349234789234923489234.png)
+     */
+    data.file.originalname = transformName;
+    const file = await this.storage.upload(data.file, 'avatar');
 
-//     const pathAvatarUser = `avatar/${data.file.originalname}`;
+    const pathAvatarUser = `avatar/${data.file.originalname}`;
 
-//     await this.userRepository.uploadAvatar(data.idUser, pathAvatarUser);
+    await this.userRepository.uploadAvatar(data.idUser, pathAvatarUser);
 
-//     return file;
-//   }
-// }
+    return file;
+  }
+}
